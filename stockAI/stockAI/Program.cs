c39fb46e0b722cca26e2deb4b40dfc126b6d6135
Heffy <83676764+Verlarium@users.noreply.github.com>
2023-01-17 20:34:29 -0500

    Add files via upload

---
+++ stockAI/stockAI/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stockAI
+{
+    class Program
+    {
+        // array indexes are by days 1 - 5 (old -> recent)
+        static double[] high = new double[] { 304.18, 302.53, 297.88, 286, 278.41 };
+        static double[] low = new double[] { 289.23, 286.13, 286.50, 272.96, 256.37 };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stock high values by old to recent: ");
+            foreach (var i in high)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Stock low values by old to recent: ");
+            foreach (var i in low)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("The average rate of change between day 1 and day 2: " + GetRateofChange(high, low, 0, 1));
+            Console.WriteLine("The net change between day 1 and day 2: " + GetNetChange(high, low, 0) / GetNetChange(high, low, 1));
+
+            Console.WriteLine("");
+            Console.WriteLine("Peak values: ");
+            foreach (var i in GetPeaks(Utils.CombineValuesByIterations(high, low, 5), 5))
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Valley values: ");
+            foreach (var i in GetValleys(Utils.CombineValuesByIterations(high, low, 5), 5))
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+
+        // Gets the rate of change from two different input arrays
+        static double GetRateofChange(double[] arr1, double[] arr2, int index1, int index2)
+        {
+            return (arr1[index1] - arr2[index1]) / (arr1[index2] - arr2[index2]);
+        }
+
+        // Gets the net change from two different input arrays
+        static double GetNetChange(double[] arr1, double[] arr2, int index)
+        {
+            return (arr1[index] - arr2[index]) / arr2[index];
+        }
+
+        // Get the highest amount of values from a input array
+        static double[] GetPeaks(double[] arr, int amount)
+        {
+            double[] vals = new double[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                vals[i] = Utils.GetHighestValue(arr);
+                arr = Utils.RemoveValue(arr, Utils.GetHighestValue(arr));
+            }
+            return vals;
+        }
+
+        // Get the lowest amount of values from a input array
+        static double[] GetValleys(double[] arr, int amount)
+        {
+            double[] vals = new double[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                vals[i] = Utils.GetLowestValue(arr);
+                arr = Utils.RemoveValue(arr, Utils.GetLowestValue(arr));
+            }
+            return vals;
+        }
+    }
+}
---
+++ stockAI/stockAI/Utils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stockAI
+{
+    static class Utils
+    {
+        // gets lowest value from input array
+        public static double GetLowestValue(double[] values)
+        {
+            double val = double.MaxValue;
+            foreach (var i in values)
+            {
+                if (i < val) val = i;
+            }
+            return val;
+        }
+
+        // gets highest value from input array
+        public static double GetHighestValue(double[] values)
+        {
+            double val = double.MinValue;
+            foreach (var i in values)
+            {
+                if (i > val) val = i;
+            }
+            return val;
+        }
+
+        // copies array and removes the input value
+        public static double[] RemoveValue(double[] values, double value)
+        {
+            double[] vals = new double[values.Length - 1];
+            int k = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] == value)
+                {
+                    continue;
+                }
+                vals[k] = values[i];
+                k++;
+            }
+            return vals;
+        }
+
+        // copies two input values and concate it together (limited)
+        public static double[] CombineValuesByIterations(double[] arr1, double[] arr2, int iterations)
+        {
+            double[] vals = new double[iterations * 2]; // arrays have to be exactly the same length
+            for (int i = 0; i < iterations; i++)
+            {
+                vals[i] = arr1[i];
+                vals[iterations + i] = arr2[i];
+            }
+            return vals;
+        }
+
+        // copies to input values and concate it together
+        public static double[] CombineValues(double[] arr1, double[] arr2)
+        {
+            double[] vals = new double[arr1.Length + arr2.Length];
+            for (int i = 0; i < arr1.Length; i++) vals[i] = arr1[i];
+            for (int i = 0; i < arr2.Length; i++) vals[arr1.Length + i] = arr2[i];
+            return vals;
+        }
+    }
+}
